f56c062098baf5e0969f301f4339693281a3f42f
Devin Lizardi <dlizardi@berkeley.edu>
2023-02-16 12:07:46 -0500

    error type checking

--- src/routes/Error.tsx
+++ src/routes/Error.tsx
@@ -1,18 +1,34 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom"
+
+type Error = {
+  statusText: string
+  message: string
+}
+
+const isError = (obj: unknown): obj is Error => "statusText" in (obj as Error) || "message" in (obj as Error)
 
 const Error = () => {
-  const error: unknown = useRouteError();
-  console.error(error);
+  const obj = useRouteError()
+  let error: Error
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  if (isError(obj)) {
+    error = obj
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    )
+  } else {
+    return (
+      <>
+        <h1>unknown error</h1>
+      </>
+    )
+  }
 }
 
 export { Error }
